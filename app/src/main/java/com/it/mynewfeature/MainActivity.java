77ad30daf5e841e00868826983f53a0e1581dbc7
chendeqiang <897164517@qq.com>
2017-10-17 15:25:39 +0800

    toolbar设置完成

--- app/src/main/java/com/it/mynewfeature/MainActivity.java
+++ app/src/main/java/com/it/mynewfeature/MainActivity.java
@@ -2,12 +2,86 @@ package com.it.mynewfeature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolBar;
+    private TextView mTv_title;
+
+    /**
+     * 在Activity中覆写该方法，用于给当前Activity添加菜单项
+     * @param menu
+     * @return
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //获取Menu布局填充器(类似于布局管理器)
+        MenuInflater menuInflater = getMenuInflater();
+        /**
+         * 参数1：菜单布局文件的id（位于res/menu目录下的资源文件）
+         * 参数2：当前Activity的菜单对象
+         * 该方法的含义是将参数1对应的菜单文件填充为菜单项后添加到当前Activity的menu中
+         */
+        menuInflater.inflate(R.menu.main_menu, menu);
+        //如果想让我们的菜单生效，返回值必须为true，否则无效
+        if (menu instanceof MenuBuilder){//如果menu是MenuBuilder类，强转为MenuBuilder类
+            MenuBuilder menuBuilder = (MenuBuilder) menu;
+            menuBuilder.setOptionalIconsVisible(true);//设置菜单图标可见
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //根据被点击的菜单的条目的id判断是哪个菜单项被点击了
+        switch (item.getItemId()) {
+            case R.id.add_friend:
+                Toast.makeText(this, "添加好友", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.share:
+                Toast.makeText(this, "分享好友", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.about:
+                Toast.makeText(this, "关于我们", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolBar = (Toolbar) findViewById(R.id.toolBar);
+        mTv_title = (TextView) findViewById(R.id.tv_title);
+        /**
+         * 注意：要想修改默认的标题，该行代码必须得在setSupportActionBar(mToolbar);之前，否则设置不成功。
+         */
+        mToolBar.setTitle("主标题");
+        setSupportActionBar(mToolBar);//将ToolBar作为ActionBar的替代者
+        mToolBar.setSubtitle("子标题");
+        mToolBar.setNavigationIcon(R.mipmap.ic_launcher);
+        /**
+         *  该行代码是设置打开系统默认的导航图标，是个箭头图标
+         *  如果设置该行代码了，那么上面代码中设置的自定义图标就会被覆盖
+         */
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //设置导航图标的点击监听
+        mToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
